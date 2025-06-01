5d66382a27f20631ead33de51cc39d857abb004d
NievaS24 <santinieva24@gmail.com>
2025-05-31 23:44:39 -0300

    avance ejercicio2

---
+++ usuarios/Program.cs
@@ -0,0 +1,53 @@
+﻿using EspacioUsuarios;
+using System.IO;
+using System.Text.Json;
+
+string rutaArchivo = "../lista.csv";
+
+// Crear una Instancia de HttpClient:
+HttpClient client = new HttpClient();
+
+// Enviar Solicitud GET: Se envía una solicitud GET a la URL especificada y se verifica que la
+// respuesta sea exitosa.
+
+string url="https://jsonplaceholder.typicode.com/users";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+
+// Leer y Deserializar la Respuesta:
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Usuarios> listUsuarios = JsonSerializer.Deserialize<List<Usuarios>>(responseBody);
+
+int contador=0; 
+
+foreach (Usuarios usuario in listUsuarios)
+{
+    if (contador < 5)
+    {
+        Console.WriteLine($"Nombre: {usuario.name}");
+        Console.WriteLine($"Correo electronico: {usuario.email}");
+        Console.WriteLine($"Domicilio: {usuario.address.street}");
+    }
+    contador++;
+}
+
+if(!File.Exists(rutaArchivo))
+{
+    File.Create(rutaArchivo);
+}
+
+using(StreamWriter writer = new StreamWriter(rutaArchivo, true))
+{
+    writer.WriteLine("Nombre, Correo Electronico");
+
+    foreach(Usuarios elementos in listUsuarios)
+    {
+        string escribirNombre = elementos.name;
+        string escribirCorreo = elementos.email;
+
+        string linea = $"{escribirNombre}, {escribirCorreo}";
+        writer.WriteLine(linea);
+    }
+
+    Console.WriteLine("Archivo cargado");
+}
---
+++ usuarios/Usuarios.cs
@@ -0,0 +1,88 @@
+namespace EspacioUsuarios;
+
+public class Geo{                                                      
+    public long lat
+    {
+        get;set;
+    }                            
+    public long lng
+    {
+        get;set;
+    }
+
+}
+
+public class Address{
+    public string street
+    {
+        get;set;
+    }
+    public string suite
+    {
+        get;set;
+    }
+    public string city
+    {
+        get;set;
+    }
+    public string zipcode
+    {
+        get;set;
+    }
+    public Geo geo
+    {
+        get;set;
+    }
+
+}
+
+public class Company{
+    public string name
+    {
+        get;set;
+    }
+    public string catchPhrase
+    {
+        get;set;
+    }
+    public string bs
+    {
+        get;set;
+    }
+}
+
+public class Usuarios
+{
+    public int id
+    {
+        get;set;
+    }
+    public string name
+    {
+        get;set;
+    }
+    public string username
+    {
+        get;set;
+    }
+    public string email
+    {
+        get;set;
+    }
+    public Address address
+    {
+        get;set;
+    }
+    public string phone
+    {
+        get;set;
+    }
+    public string website
+    {
+        get;set;
+    }
+    public Company company
+    {
+        get;set;
+    }
+}                                                                                                                                                                                                                                          
